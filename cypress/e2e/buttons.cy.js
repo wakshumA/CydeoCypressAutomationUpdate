69a6ac2107763d89b86f1464dd64560e221ccaad
OscarSah <oscar@cybertekschool.com>
2023-05-07 18:55:28 +0300

    add: new locators strategies

--- cypress/e2e/buttons.cy.js
+++ cypress/e2e/buttons.cy.js
@@ -10,6 +10,15 @@ describe('Context: My First Tests', () => {
         // select a button with text
         cy.contains('Button 2').should('be.visible').click();
         cy.contains('Clicked on button two!').should('be.visible');
+
+        // find element with class attribute and create a list then select 3rd element from the list
+        cy.get('.btn.btn-primary').then(($buttons) => {
+            cy.wrap($buttons).eq(2).click(); // zero indexed list now
+            // in List Java, list.get(index)
+            // assert the text
+            cy.contains('Clicked on button three!').should('be.visible');
+        })
+
     })
   
 })
