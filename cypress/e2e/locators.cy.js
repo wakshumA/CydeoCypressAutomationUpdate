47d11ba11c6f4f08e0994f3e585e75d8239a8613
OscarSah <oscar@cybertekschool.com>
2023-05-07 18:11:33 +0300

    add: new locators strategies

--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -34,5 +34,25 @@ describe('Find or Get Elements by Using Different Locators', () => {
     
     })
 
+    it('Check finding elements by traveling through DOM', () => {
+        // travel to find the login button: locate username box- go to parent form -then find button
+        cy.get('input[name="username"]').parents('form').find('button').should('contain','Login').click();
+
+    })
+
+    it.only('Check Different Type of Assertions', () => {
+        // Cypress itself bundles assertions provided by Chai, Sinon and jQuery libraries
+        // Should Assertion: does the assertion directly on the object itself
+        cy.get('#wooden_spoon')
+        .should('contain','Login')
+        .and('have.class','btn btn-primary');
+        // expect assertion: creates a subject of our test, then you implement different actions 
+        cy.get('#wooden_spoon').then((buttonElement) => {
+            expect(buttonElement).to.have.text('Login');
+            expect(buttonElement).to.have.class('btn btn-primary');
+        })
+    })
+
+
     
 })
