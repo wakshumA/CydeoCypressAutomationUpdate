d808bbf15df1f8228478ca69c721e8d4afde0c41
OscarSah <oscar@cybertekschool.com>
2023-05-07 17:10:30 +0300

    add: new locators strategies

--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -13,11 +13,26 @@ describe('Find or Get Elements by Using Different Locators', () => {
        // attribute name and value
        cy.get("[type='text']").clear();  // clear what is typed
 
+       // tagName
        cy.get("input").each((item, index, list) => {
         // assert the length of the list is 2
         expect(list).to.have.length(2);
         expect(item).to.have.attr("type");
        })
+       
+       // by attribute name
+       cy.get('[type]');
+
+       // by className
+       cy.get('.btn.btn-primary');
+
+       // By id
+       cy.get('#wooden_spoon');
+
+       // if I want to use text: no xpath in cypress, but it still possible with a different approach
+       cy.get('button').should('contain','Login').click();
+    
     })
+
     
 })
