5df1a740e9472e6b0f63d698a36440dd5fd572a5
OscarSah <oscar@cybertekschool.com>
2023-05-07 17:21:33 +0300

    add: new locators strategies

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://practice.cydeo.com/',
+    video: false,
+    retries: 2,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
