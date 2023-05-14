a9ba3cf5909f7e38562b12023da098e28f70e188
oscar <oscar@cybertekschool.com>
2023-05-14 17:01:45 +0300

    add: webtables

---
+++ cypress/e2e/webTables.cy.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+describe('Cypress WebTable Tests', () => {
+    /**
+     * 
+     */
+     beforeEach('Navigate to upload page', () => {
+       cy.clearCookies();
+       cy.visit('/webtables');
+     });
+    
+     it('Check finding and editing a record', () => {
+       
+     });
+    
+   });
