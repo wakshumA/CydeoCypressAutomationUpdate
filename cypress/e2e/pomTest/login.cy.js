5c23ec2383f16f967b3bd83b797548bd6cba2160
oscar <oscar@cybertekschool.com>
2023-05-20 17:19:39 +0300

    created POM files

--- cypress/e2e/pomTest/login.cy.js
+++ cypress/e2e/pomTest/login.cy.js
@@ -1,3 +1,10 @@
 import { auth } from '../../support/pages/auth'
 import { navigateTo } from '../../support/pages/navigation'
 
+describe('Auth: Login user with different ways', () => {
+    // navigation to the test page
+    beforeEach('navigate to login page', () => {
+        cy.clearAllCookies();
+        navigateTo.loginPage(); // this function we called it from our POM
+    })
+} )
